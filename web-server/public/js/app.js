3a619126bf665f3ba6ae941618c1a06407909d33
zamdi1 <zamdi@student.wethinkcode.co.za>
2021-01-02 16:19:17 +0200

    rendering

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,14 +1,40 @@
 console.log("client side javascript file is loaded up")
 
-fetch('http://localhost:3000/weather?address=Springs')
-    .then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                console.log(data.error)
-            } else {
 
-                console.log(data.location)
-                console.log(data.forecast)
-            }
-        })
-    });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+messageOne.textContent = '';
+messageTwo.textContent = '';
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value;
+
+
+    if (location !== '') {
+        fetch(`http://localhost:3000/weather?address=${location}`)
+            .then((response) => {
+                response.json().then((data) => {
+                    if (data.error) {
+                        // console.log(data.error)
+                        messageOne.textContent = data.error;
+                    } else {
+
+                        // console.log(data.location)
+                        messageOne.textContent = data.location
+                        // console.log(data.forecast)
+                        messageTwo.textContent = data.forecast
+                    }
+                })
+            });
+
+    } else {
+        // console.log('You must provide an address')
+        messageOne.textContent = 'You must provide an address'
+    }
+    // console.log(location);
+})
