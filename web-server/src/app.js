1d0465857b8913b07ee978b83b82633238149eb6
zamdi1 <zamdi@student.wethinkcode.co.za>
2021-01-02 06:20:18 +0200

    zakhele working with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,34 @@
 const path = require('path')
 const express = require('express');
-const {
-    resolve
-} = require('path');
+// const {
+//     resolve
+// } = require('path');
+const hbs = require('hbs');
 
 
 // console.log(__dirname);
 // console.log(path.join(__dirname,'../public'));
 
+//Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express();
 
 
 
 
-//index.html has a special meaning this will run automatically
-//and it will render the index.html
+//Setup handlebars engine and views locaion
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath)); //we use this when 
 //we are handling static html files
 // app.set('views', __dirname + '/views');
+
 
 app.get('', (req, res) => { //we use this when we're handling dynamic html files
     res.render('index', {
@@ -29,8 +37,19 @@ app.get('', (req, res) => { //we use this when we're handling dynamic html files
     })
 })
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        name: 'Zakhele',
+        title: 'Help'
+    })
+})
+
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {
+        name: 'Zakhele Madi',
+        title: 'About me'
+    })
 })
 
 // app.get('',(req,res)=>{
