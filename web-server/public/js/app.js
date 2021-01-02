d34b59c68b67deb850ad5ce844598e1b3ae1ca64
zamdi1 <zamdi@student.wethinkcode.co.za>
2021-01-02 17:15:10 +0200

    okay

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,14 +7,14 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
-messageOne.textContent = '';
-messageTwo.textContent = '';
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value;
 
 
+    messageOne.textContent = 'Loading';
+    messageTwo.textContent = '';
     if (location !== '') {
         fetch(`http://localhost:3000/weather?address=${location}`)
             .then((response) => {
